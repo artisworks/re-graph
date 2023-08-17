b05e50ab9288e1670afadb7ac589b7bb9c69a740
rhishikeshj <rhishikeshj@gmail.com>
2023-08-17 16:57:52 +0200

    Minor code improvements.
    1. Make fns private
    2. Use subprotocols from the create-ws args
    3. Add try-catch around read-message

--- aleph/src/re_graph/interop.clj
+++ aleph/src/re_graph/interop.clj
@@ -2,27 +2,41 @@
   (:require
    [aleph.http :as http]
    [byte-streams]
+   [clojure.string :as str]
    [clojure.tools.logging :as log]
    [jsonista.core :as json]
    [manifold.deferred :as d]
    [manifold.stream :as stream]))
 
-(defn reconnect-after [timeout f]
-  (future
-    (Thread/sleep timeout)
-    (f)))
-
-(defn read-message [msg]
+(defn- read-message [msg]
   (-> msg
       byte-streams/to-string
       (json/read-value json/keyword-keys-object-mapper)))
 
-(defn read-ws-message [msg]
+(defn- read-ws-message [msg]
   (let [data (byte-streams/to-string msg)]
     data))
 
-;; TODO: Read sub-protocols from callbacks
-(defn create-ws [url {:keys [on-open on-message on-error on-close] :as callbacks}]
+(defn- handle-response [success failure data]
+  (let [msg (try (read-message (:body data))
+                 (catch Exception e
+                   (log/error "Error while reading message " e)
+                   (failure e)
+                   ::read-message-error))]
+    (when (not= ::read-message-error msg)
+      (success (assoc data :body msg)))))
+
+(defn- handle-error [callback data]
+  (log/error "Received response error " (:status data))
+  (callback data))
+
+;; ──────────────────────────────────── API ───────────────────────────────────
+
+(defn create-ws [url {:keys [on-open
+                             on-message
+                             on-close
+                             on-error
+                             subprotocols]}]
   (log/info "Connecting to " url)
   (try
     (let [conn @(http/websocket-client
@@ -30,37 +44,24 @@
                  {:max-frame-size 9999999
                   :max-frame-payload 9999999
                   :compression? true
-                  :sub-protocols "graphql-ws"
+                  :sub-protocols (str/join "," subprotocols)
                   :heartbeats {:send-after-idle 20000
                                :timeout 10000}})]
       (log/info "Connected to " url)
       (stream/consume (comp on-message read-ws-message) conn)
-      (stream/on-closed conn (fn [] (on-close 5001 "closed")))
+      (stream/on-closed conn (fn [] (on-close)))
       (on-open conn))
     (catch Exception e
-      ;; TODO: check if this is best logic. Currently if we try to reconnect
-      ;; and some thing happens (transient DNS error) then we raise on-error
-      ;; and without calling on-close it would not retry again.
       (on-error e)
-      (on-close 5001 "Closed"))))
+      (on-close))))
 
 (defn send-ws [conn payload]
-  (log/info "Sending ws payload out")
   @(d/timeout! (stream/put! conn payload) 2000))
 
 (defn close-ws [conn]
   (stream/close! conn))
 
-(defn handle-response [success failure data]
-  (log/info "Received response " (:status data))
-  (success (update data :body read-message)))
-
-(defn handle-error [callback data]
-  (log/info "Received response error " (:status data))
-  (callback data))
-
 (defn send-http [url request payload on-success on-error]
-  (log/info "send-http" {:url url :request request :payload payload})
   (let [req (-> request
                 (update :headers assoc  "Content-Type" "application/json" "Accept" "application/json")
                 (assoc :body payload))]
